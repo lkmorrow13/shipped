5cc0aca1ba65b24b5e54b36c2b3b55aac73cc031
Laven Morrow <lmorrow13@live.com>
2018-03-02 11:53:07 -0700

    set rescources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'boat_jobs/index'
-  get 'boat_jobs/new'
-  get 'boat_jobs/edit'
-  get 'boat_jobs/update'
-  get 'boat_jobs/create'
-  get 'boat_jobs/show'
-  get 'boat_jobs/destroy'
-  get 'jobs/index'
-  get 'jobs/new'
-  get 'jobs/edit'
-  get 'jobs/update'
-  get 'jobs/create'
-  get 'jobs/show'
-  get 'jobs/destroy'
-  get 'boats/index'
-  get 'boats/new'
-  get 'boats/edit'
-  get 'boats/update'
-  get 'boats/create'
-  get 'boats/show'
-  get 'boats/destroy'
-  get 'home/index'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  devise_for :users
+  resources :jobs
+  resources :boats
+  resources :boat_jobs
+  
   root 'home#index'
 end
