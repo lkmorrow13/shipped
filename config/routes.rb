747606077bd53a0fe1ff3f7713e249b4669475fb
Laven Morrow <lmorrow13@live.com>
2018-03-02 15:52:17 -0700

    finished build of layout page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+  resources :users, only: [:index, :show]
   resources :jobs
   resources :boats
   resources :boat_jobs
-  
+
   root 'home#index'
 end
