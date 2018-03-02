f19333bf84eabc99f340be6498842a90c3f1cd98
Laven Morrow <lmorrow13@live.com>
2018-03-02 10:26:31 -0700

    built boat model and boats controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'boats/index'
+  get 'boats/new'
+  get 'boats/edit'
+  get 'boats/update'
+  get 'boats/create'
+  get 'boats/show'
+  get 'boats/destroy'
   get 'home/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
