a4faf3bd1a2b9c33b5950354ce520b87e7ebe893
Laven Morrow <lmorrow13@live.com>
2018-03-02 14:40:26 -0700

    building controllers

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,25 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
 
   def new
+    @job = Job.new
   end
 
   def edit
+    @job = Job.find(params[:id])
   end
 
   def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to @job
   end
 
   def create
+    @job = Job.create(user_params)
+    redirect_to @user
   end
 
   def show
